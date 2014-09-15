7fd63ba6466b386e7b74b6ff5c7b4c75a7b3c9a3
Luciano.Federico.Olmedo <Luciano.Federico.Olmedo@gmail.com>
2014-09-15 19:29:04 +0000

    La BD es congruente con el diagrama ER

--- aterrizar2/src/main/resources/createAll.sql
+++ aterrizar2/src/main/resources/createAll.sql
@@ -19,19 +19,19 @@ CREATE TABLE  `Epers_EjHibernate`.`Aerolinea` (
   `A_ID` int(11) NOT NULL AUTO_INCREMENT,
   `A_NOMBRE` varchar(20) NOT NULL unique,
   `A_IDE` int(11) NOT NULL ,
+  PRIMARY KEY (`A_ID`),
   FOREIGN KEY (A_IDE) 
     REFERENCES Empresa(E_ID) 
     ON DELETE CASCADE
-  PRIMARY KEY (`A_ID`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
 
 CREATE TABLE  `Epers_EjHibernate`.`Vuelo` (
   `V_ID` int(11) NOT NULL AUTO_INCREMENT,
   `V_IDA` int(11) NOT NULL ,
+   PRIMARY KEY (`V_ID`),
   FOREIGN KEY (V_IDA) 
     REFERENCES Aerolinea(A_ID) 
-    ON DELETE CASCADE
-  PRIMARY KEY (`V_ID`)
+    ON DELETE CASCADE  
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
 
 CREATE TABLE  `Epers_EjHibernate`.`Tramo` (
@@ -40,12 +40,12 @@ CREATE TABLE  `Epers_EjHibernate`.`Tramo` (
   `T_DESTINO` varchar(20) NOT NULL,
   `T_HORASALIDA` int(11) NOT NULL,
   `T_HORALLEGADA` int(11) NOT NULL,
-  `T_PRECIO`int(11) NOT NULL
+  `T_PRECIO`int(11) NOT NULL,
   `T_IDV` int(11) NOT NULL ,
-  FOREIGN KEY (T_IDV) 
+    PRIMARY KEY (`T_IDV`),
+  FOREIGN KEY (T_ID) 
     REFERENCES Vuelo(V_ID) 
     ON DELETE CASCADE
-  PRIMARY KEY (`T_ID`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
 
 CREATE TABLE  `Epers_EjHibernate`.`Asiento` (
