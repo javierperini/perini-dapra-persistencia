f4ee339c0619248a53aa21424d8371d55537c58f
Luciano.Federico.Olmedo@gmail.com <Luciano.Federico.Olmedo@gmail.com@6ec1abf4-0aa4-0f3a-b079-5cce9fa72fab>
2014-09-15 19:52:41 +0000

    Agregando xml ss

--- trunk/aterrizar2/src/main/resources/createAll.sql
+++ trunk/aterrizar2/src/main/resources/createAll.sql
@@ -3,10 +3,10 @@ CREATE SCHEMA Epers_EjHibernate;
 
 USE Epers_EjHibernate;
 
-CREATE TABLE  `Epers_EjHibernate`.`Usuarios` (
-  `J_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `J_NOMBRE` varchar(20) NOT NULL,
-  PRIMARY KEY (`J_ID`)
+CREATE TABLE  `Epers_EjHibernate`.`Usuario` (
+  `U_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `U_NOMBRE` varchar(20) NOT NULL,
+  PRIMARY KEY (`U_ID`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
 
 CREATE TABLE  `Epers_EjHibernate`.`Empresa` (
