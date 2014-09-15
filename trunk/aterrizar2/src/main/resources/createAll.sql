c610442e3210775f7350721d59f14509032dd962
Luciano.Federico.Olmedo@gmail.com <Luciano.Federico.Olmedo@gmail.com@6ec1abf4-0aa4-0f3a-b079-5cce9fa72fab>
2014-09-15 19:19:36 +0000

    Actualizando la BD ...

--- trunk/aterrizar2/src/main/resources/createAll.sql
+++ trunk/aterrizar2/src/main/resources/createAll.sql
@@ -10,8 +10,56 @@ CREATE TABLE  `Epers_EjHibernate`.`Usuarios` (
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
 
 CREATE TABLE  `Epers_EjHibernate`.`Empresa` (
-  `J_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `J_NOMBRE` varchar(20) NOT NULL,
-  PRIMARY KEY (`J_ID`)
+  `E_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `E_NOMBRE` varchar(20) NOT NULL unique,
+  PRIMARY KEY (`E_ID`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE  `Epers_EjHibernate`.`Aerolinea` (
+  `A_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `A_NOMBRE` varchar(20) NOT NULL unique,
+  `A_IDE` int(11) NOT NULL ,
+  FOREIGN KEY (A_IDE) 
+    REFERENCES Empresa(E_ID) 
+    ON DELETE CASCADE
+  PRIMARY KEY (`A_ID`)
+) ENGINE = InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE  `Epers_EjHibernate`.`Vuelo` (
+  `V_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `V_IDA` int(11) NOT NULL ,
+  FOREIGN KEY (V_IDA) 
+    REFERENCES Aerolinea(A_ID) 
+    ON DELETE CASCADE
+  PRIMARY KEY (`V_ID`)
+) ENGINE = InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE  `Epers_EjHibernate`.`Tramo` (
+  `T_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `T_ORIGEN` varchar(20) NOT NULL,
+  `T_DESTINO` varchar(20) NOT NULL,
+  `T_HORASALIDA` int(11) NOT NULL,
+  `T_HORALLEGADA` int(11) NOT NULL,
+  `T_PRECIO`int(11) NOT NULL
+  `T_IDV` int(11) NOT NULL ,
+  FOREIGN KEY (T_IDV) 
+    REFERENCES Vuelo(V_ID) 
+    ON DELETE CASCADE
+  PRIMARY KEY (`T_ID`)
+) ENGINE = InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE  `Epers_EjHibernate`.`Asiento` (
+  `S_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `S_CAT_PRECIO` varchar(20) NOT NULL unique,
+  `S_USUARIOID` int(11),
+  PRIMARY KEY (`S_ID`)
+) ENGINE = InnoDB AUTO_INCREMENT=1;
+
+
+
+
+
+
+
+
 
