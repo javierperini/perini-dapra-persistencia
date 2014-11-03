f976d7f81cdebc06a3cda0386b63a1a0043cb0b4
javierperini90 <javierperini90@gmail.com>
2014-11-03 22:47:15 +0000

    Frula3

--- aterrizar2/src/main/java/unq/tpi/persistencia/busquedas/Criterio.java
+++ aterrizar2/src/main/java/unq/tpi/persistencia/busquedas/Criterio.java
@@ -8,12 +8,12 @@ import unq.tpi.persistencia.Aerolinea;
 import unq.tpi.persistencia.Categoria;
 import unq.tpi.persistencia.Entidad;
 
-public class Criterio extends Entidad {
+public abstract class Criterio extends Entidad {
 	
 	private String criterio;
-	private List<Criterio> criteriosComplementarios = new ArrayList<Criterio>();
+	protected List<Criterio> criteriosComplementarios = new ArrayList<Criterio>();
 
-	private Criterio(String criterio){
+	public Criterio(String criterio){
 		this.criterio = criterio;
 	}
 	
@@ -27,21 +27,9 @@ public class Criterio extends Entidad {
 	
 	// Metodos de manipulaci�n de los criterios
 	
-	public void and(Criterio otroCriterio){
-		criteriosComplementarios.add(otroCriterio.and());
-	}
+	public abstract void and(Criterio otroCriterio);
 	
-	public void or(Criterio otroCriterio){
-		criteriosComplementarios.add(otroCriterio.or());
-	}
 	
-	private Criterio and(){
-		return new Criterio(" and ".concat(this.criterio));
-	}
-	
-	private Criterio or(){
-		return new Criterio(" or ".concat(this.criterio));
-	}
 
 	public void removeLast() throws NoSePuedeEliminarCriterioDeBusquedaException{
 		try{
@@ -52,42 +40,8 @@ public class Criterio extends Entidad {
 		}
 	}
 	
-	// Factory Methods para crear diferentes criterios de busqueda
-	
-	public static Criterio busquedaPorAerolinea(Aerolinea unaAerolinea){
-		return new Criterio("aerolinea = ".concat(unaAerolinea.getNombre()));
-	}
-	
-	public static Criterio busquedaPorCategoriaDeAsiento(Categoria unaCategoria){
-		return new Criterio("categoria = ".concat(unaCategoria.getNombre()));
-	}
-	
-	public static Criterio busquedaPorFechaDeSalida(Integer unaFechaDeSalida){
-		return new Criterio("fechaDeSalida = ".concat(unaFechaDeSalida.toString()) );
-	}
-	
-	public static Criterio busquedaPorFechaDeLlegada(Integer unaFechaDeLlegada){
-		return new Criterio("fechaDeLlegada = ".concat(unaFechaDeLlegada.toString()));
-	}
-	
-	public static Criterio busquedaPorOrigen(String unOrigen){
-		return new Criterio("");
-	}
-	
-	public static Criterio busquedaPorDestino(String unDestino){
-		return new Criterio("");
-	}
 	
 	
-	public static void main(String args[]){
-		Criterio c = new Criterio("criterio1");
-		Criterio d = new Criterio("criterio2");
-		Criterio e = new Criterio("criterio3");
-		c.and(d);
-		c.or(e);
-		c.and(busquedaPorAerolinea(new Aerolinea("LAN")));
-		
-		System.out.println(c.getCriterio());
-	}
 	
+//	
 }
